40205a25a03041ed1c60bf0d5cbd49445bb56b5f
kushagrjolly15 <kushagrjolly15@gmail.com>
2016-04-12 00:33:46 +0530

    v1.0
    No changes.

--- Application/build/generated/source/r/debug/com/example/android/customchoicelist/R.java
+++ Application/build/generated/source/r/debug/com/example/android/customchoicelist/R.java
@@ -11,16 +11,16 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
-        public static final int horizontal_page_margin=0x7f040002;
-        public static final int margin_huge=0x7f040003;
-        public static final int margin_large=0x7f040004;
-        public static final int margin_medium=0x7f040005;
-        public static final int margin_small=0x7f040006;
-        public static final int margin_tiny=0x7f040007;
-        public static final int page_margin=0x7f040008;
-        public static final int vertical_page_margin=0x7f040009;
+        public static final int activity_horizontal_margin=0x7f040002;
+        public static final int activity_vertical_margin=0x7f040003;
+        public static final int horizontal_page_margin=0x7f040000;
+        public static final int margin_huge=0x7f040004;
+        public static final int margin_large=0x7f040005;
+        public static final int margin_medium=0x7f040006;
+        public static final int margin_small=0x7f040007;
+        public static final int margin_tiny=0x7f040008;
+        public static final int page_margin=0x7f040009;
+        public static final int vertical_page_margin=0x7f040001;
     }
     public static final class drawable {
         public static final int iari_library=0x7f020000;
@@ -60,21 +60,21 @@ public final class R {
         public static final int menu_splash_screen=0x7f070002;
     }
     public static final class string {
-        public static final int action_settings=0x7f050000;
-        public static final int app_name=0x7f050001;
-        public static final int hello_world=0x7f050002;
-        public static final int intro_message=0x7f050003;
-        public static final int title_activity_intermediate=0x7f050004;
-        public static final int title_activity_pending_work=0x7f050005;
-        public static final int title_activity_splash_screen=0x7f050006;
+        public static final int action_settings=0x7f060000;
+        public static final int app_name=0x7f060001;
+        public static final int hello_world=0x7f060002;
+        public static final int intro_message=0x7f060003;
+        public static final int title_activity_intermediate=0x7f060004;
+        public static final int title_activity_pending_work=0x7f060005;
+        public static final int title_activity_splash_screen=0x7f060006;
     }
     public static final class style {
-        public static final int AppTheme=0x7f060000;
-        public static final int Theme_Base=0x7f060001;
-        public static final int Theme_Sample=0x7f060002;
-        public static final int Widget=0x7f060003;
-        public static final int Widget_DescriptionBar=0x7f060004;
-        public static final int Widget_SampleMessage=0x7f060005;
-        public static final int Widget_SampleMessageTile=0x7f060006;
+        public static final int AppTheme=0x7f050002;
+        public static final int Theme_Base=0x7f050001;
+        public static final int Theme_Sample=0x7f050003;
+        public static final int Widget=0x7f050004;
+        public static final int Widget_DescriptionBar=0x7f050005;
+        public static final int Widget_SampleMessage=0x7f050000;
+        public static final int Widget_SampleMessageTile=0x7f050006;
     }
 }
